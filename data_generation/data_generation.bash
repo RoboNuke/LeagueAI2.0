a726af47b689902dc9874693eb2e14614e66ee07
RoboNuke <hunter.brown4762@gmail.com>
2020-09-06 13:14:06 -0700

    Added split script and finalized it

--- data_generation/data_generation.bash
+++ data_generation/data_generation.bash
@@ -12,16 +12,23 @@ export skipFrames=40
 export datasetFile="data/3data_set/"
 export countFile="count.yaml"
 export mapFile="data/0map/"
-export datasetSize=10
+export datasetSize=11
 export labelFile="cfg/vayne.names"
-#python3 scripts/video_generator.py -c "$vidConfigFile" -o "$vidFile" -p "$vidPrefix"
 
+# split vars
+export datasetFinal="/home/hunter/Games/vayneDataset/"
+export trainingSetSize=8
+export datasetCfg="/home/hunter/Games/cfg/"
+export datasetName="vayneTest"
+export top=2
+
+#python3 scripts/video_generator.py -c "$vidConfigFile" -o "$vidFile" -p "$vidPrefix"
 
 # Remove background, create images of sprite only
 #python3 scripts/frameExporter.py -o "$cropFile" -c "$vidConfigFile" -p "$cropPrefix" -i "$vidFile" -s "$skipFrames"  -q "$vidPrefix"
 
 # Generate the data set
-python3 scripts/bootstrap.py -c "$vidConfigFile" -o "$datasetFile" -k "$countFile" -i "$cropFile" -q "$cropPrefix" -m "$mapFile" -n "$datasetSize" -l "$labelFile"
+#python3 scripts/bootstrap.py -c "$vidConfigFile" -o "$datasetFile" -k "$countFile" -i "$cropFile" -q "$cropPrefix" -m "$mapFile" -n "$datasetSize" -l "$labelFile"
 
 # organize the dataset for training
-#split_test_train.py
+python3 scripts/split_train_test_darknet_style.py -i "$datasetFile" -c "$vidConfigFile" -o "$datasetFinal" -s "$trainingSetSize" -k "$datasetCfg" -n "$datasetName" -t "$top"
