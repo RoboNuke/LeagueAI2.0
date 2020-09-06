866429f80f9e10eb3cd2bddf2c0dadc7d4fd8757
RoboNuke <hunter.brown4762@gmail.com>
2020-09-06 13:14:48 -0700

    Tested and works

--- data_generation/scripts/split_train_test_darknet_style.py
+++ data_generation/scripts/split_train_test_darknet_style.py
@@ -1,6 +1,7 @@
-from os import listdir
+import os, sys, getopt
 import numpy as np
 import random
+import yaml
 
 # This script is used to split the datasets generated with bootstrap.py into test and training dataset
 
@@ -16,15 +17,47 @@ import random
 # -- test.txt
 
 
-datasetSize = 10
-trainSetSize=2
+trainSetSize=8
 inputFile="../data/3data_set/"
 outputFile="/home/hunter/LeagueAI2.0/data_generation/data/4darknet_ready/"
-outConfig="/home/hunter/LeagueAI2.0/data_generation/cfg/testSet.cfg"
+outConfig="/home/hunter/LeagueAI2.0/data_generation/cfg/"
 configFile="../cfg/vayne.yaml"
+datasetName = "vayneTest"
 topSize=2
+numClasses = 2
 
 
+def unpack(argv):
+    global trainSetSize, inputFile, outputFile, outConfig, configFile, datasetName, topSize
+    try:
+        print(argv)
+        opts, args = getopt.getopt(argv, "hi:c:o:s:k:n:t:", ["inputFile=", "configFile=", "outputFile=", "trainingSetSize=", "outputConfigFile=", "datasetName=", "top="])
+    except getopt.GetoptError:
+        print(getopt.GetoptError)
+        print("split_train_test_darknet_style.py -i <input file> -c <configuration file> -o <output file> -s <desired training set size> -k <output cfg file> -n <dataset Name> -t <top n>")
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt == "-h":
+            print("split_train_test_darknet_style.py -i <input file> -c <configuration file> -o <output file> -s <desired training set size> -k <output cfg file> -n <dataset Name> -t <top n>")
+            sys.exit()
+        elif opt in ("-c", "configFile="):
+            configFile = arg
+        elif opt in ("-o","outputFile="):
+            outputFile = arg
+        elif opt in ("-s","trainingSetSize="):
+            outFilePrefix = int(arg)
+        elif opt in ("-i","inputFile="):
+            inputFile = arg
+        elif opt in ("-k", "outputConfigFile="):
+            outConfig = arg
+        elif opt in ("-n", "datasetName"):
+            datasetName = arg
+        elif opt in ("-t", "top="):
+            topSize = int(arg)
+    labels = yaml.load(open(configFile))
+    numClasses = len(labels["champions"]) + len(labels["creatures"])
+            
 def splitData(dataset):
     global trainSetSize
     random.shuffle(dataset)
@@ -43,44 +76,51 @@ def copyData(trainFiles, testFiles):
         os.makedirs(outputFile + "train/images")
     newTrain = []
     newTest = []
+
+    os.system('cp ' + inputFile + "labels.txt " + outputFile + "labels.txt")
     for f in trainFiles:
-        k = f[-4]
-        os.system('cp ' + f + " " + outputFile + "train/images/image" + k + ".jpg")
-        os.system('cp ' + inputFile + "labels/label" + k + ".txt " + outputFile + "train/labels/label" + k + ".txt")
-        newTrain.append(outputFile + "train/images/image" + k + ".jpg")
+        k = f[5:-4]
+        os.system('cp ' + inputFile + "images/" + f + " " + outputFile + "train/images/" + datasetName + k + ".jpg")
+        os.system('cp ' + inputFile + "labels/label" + k + ".txt " + outputFile + "train/labels/" + datasetName + k + ".txt")
+        newTrain.append(outputFile + "train/images/" + datasetName + k + ".jpg")
     for f in testFiles:
-        k = f[-4]
-        os.system('cp ' + f + " " + outputFile + "test/images/image" + k + ".jpg")
-        os.system('cp ' + inputFile + "labels/label" + k + ".txt " + outputFile + "test/labels/label" + k + ".txt")
-        newTest.append(outputFile + "test/images/image" + k + ".jpg")
+        k = f[5:-4]
+        os.system('cp ' + inputFile + "images/" + f + " " + outputFile + "test/images/" + datasetName + k + ".jpg")
+        os.system('cp ' + inputFile + "labels/label" + k + ".txt " + outputFile + "test/labels/" + datasetName + k + ".txt")
+        newTest.append(outputFile + "test/images/" + datasetName + k + ".jpg")
+    print("Completed copying files to new location")
     return newTrain, newTest
-        
-        
     
 def makeListFile(trainFiles, testFiles):
     global outputFile
-    if not os.path.exists(outputFile + "test.list"):
-        os.makedirs(outputFile + "test.list")
-    if not os.path.exists(outputFile + "train.list"):
-        os.makedirs(outputFile + "train.list")
+    if not os.path.exists(outputFile):
+        os.makedirs(outputFile)
     with open(outputFile + "test.list", "w") as f:
-        for fil in testFiles::
-            f.write(testFiles + "\n")
+        for fil in testFiles:
+            f.write(fil + "\n")
     with open(outputFile + "train.list", "w") as f:
         for fil in trainFiles:
             f.write(fil+"\n")
+    print("List Files Made")
 
-def makeLabelFile(path):
-    global outConfig, topSize
-    if not os.path.exists(outConfig):
-        os.makedirs(outConfig)
-    with open(outConfig, "w") as f:
-        f.write("classes=" + numClasses + "\n")
+def makeLabelFile():
+    global outConfig, topSize, datasetName
+    with open(outConfig + datasetName + ".cfg", "w") as f:
+        f.write("classes=" + str(numClasses) + "\n")
         f.write("train = " + outputFile + "train.list\n")
         f.write("valid = " + outputFile + "test.list\n")
         f.write("labels = " + outputFile + "labels.txt\n")
         f.write("backup = backup/\n")
         f.write("top=" + str(topSize) + "\n")
+    print("Label File Made")
 
 if __name__ == "__main__":
+    print("\nStarting to split dataset and copy to final locations")
+    # get a list of all the files in the training set
+    unpack(sys.argv[1:])
+    dataset = os.listdir(inputFile + "images/")
+    train, test = splitData(dataset)
+    train, test = copyData(train, test)
+    makeListFile(train, test)
+    makeLabelFile()
     
